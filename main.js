fa97c63d4e608fcadfeeaae0ece0eb48f94f3de5
Numaira222 <77472414+Numaira222@users.noreply.github.com>
2022-11-06 12:32:51 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
 objects = [];
 status = "";
 function setup(){
-    canvas = createCanvas(380,280);
+    canvas = createCanvas(280,180);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
 }
 function draw(){
-    image(video,0,0,380,280);
+    image(video,0,0,280,180);
     
-    if(status != ""){
-    object_detector.detect(gotResults);
-    for(i=0; i<objects.length; i++){
-        document.getElementById("status").innerHTML = "Status: Object Detected";
-        document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected are:"+ objects.length;
-        fill("red");
-        percent = floor(objects[i].confidence*100);
-        text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
-        noFill();
-        stroke("red");
-        rect(objects[i].x ,objects[i].y ,objects[i].width, objects[i].height);
+}
 
-    }
-}
-}
+
 function start(){
     object_detector = ml5.objectDetector("cocossd", modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
